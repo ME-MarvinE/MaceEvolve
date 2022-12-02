801cfa018cd7869d10178e8f9480ad487f2bb8fb
Marvin E <Marvin.Etuk@gmail.com>
2022-12-02 02:44:20 +0000

    Fix compile error introduced in previous commit

--- MaceEvolve.Core/Interfaces/IRectangle.cs
+++ MaceEvolve.Core/Interfaces/IRectangle.cs
@@ -2,9 +2,9 @@
 {
     public interface IRectangle
     {
-        public double X { get; set; }
-        public double Y { get; set; }
-        public double Width { get; set; }
-        public double Height { get; set; }
+        double X { get; set; }
+        double Y { get; set; }
+        double Width { get; set; }
+        double Height { get; set; }
     }
 }
