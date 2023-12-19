b6cbce6abb9f87dcb775161bbe1370e09c5c481f
Marvin E <Marvin.Etuk@gmail.com>
2023-12-19 00:25:30 +0000

    Fix simulation window not opening in MaceEvolve.SilkGL

--- MaceEvolve.SilkGL/Program.cs
+++ MaceEvolve.SilkGL/Program.cs
@@ -2,9 +2,11 @@
 {
     public class Program
     {
+        #region Methods
         static void Main(string[] args)
         {
-
+            MaceEvolveApp silkMace = new MaceEvolveApp();
         }
+        #endregion
     }
 }
